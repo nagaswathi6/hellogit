8e76bcda475da73b14a90300e16a0e2bd23c5609
krishnareddyML <krishnareddy.nandyala@gmail.com>
2022-11-16 22:59:24 +0530

    Initial Commit

---
+++ src/test/java/com/example/hellogit/HellogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
